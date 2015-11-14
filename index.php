8376f7988955a478e48e06a3c44c1735ebfd8236
smltng <sat83@cornell.edu>
2015-11-14 14:39:08 -0500

    linking to bootstrap

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/bootstrap.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
 <body>
-	<?php echo("Hello world");?>
+
 </body>
 </html>
