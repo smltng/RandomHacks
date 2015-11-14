a963f76bee969a14e1ceb2ec5e6d3e22a901cb40
smltng <sat83@cornell.edu>
2015-11-14 14:41:28 -0500

    oops lines were backwards

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="js/bootstrap.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    
 </head>
 <body>
 
