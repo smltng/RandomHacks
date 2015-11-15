edc968132c8c528f291df09c39f39cdc3d05bab9
danny-sindra <danny.sindra@gmail.com>
2015-11-14 22:45:37 -0500

    view: add comment

--- js/view.js
+++ js/view.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
  });
 
 function fetchComplaintData(){
-
 	var url=window.location.search;
 	var id=""+url.substring(4);
 
@@ -15,6 +14,7 @@ function fetchComplaintData(){
 		success: function(complaint) {
 			$('#subject').text(complaint.get("subject"));
 			$('#description').text(complaint.get("description"));
+      setTagValue('#tags', complaint);
 
       retrieveVoteData(complaint);
       retrieveCommentData(complaint);
@@ -30,10 +30,14 @@ function retrieveCommentData(complaint) {
   //get comment data
   var Comment = Parse.Object.extend('Comment');
   var qComment = new Parse.Query(Comment);
-  qVoteUser.equalTo('complaintId', complaint);
-  qVoteUser.find({
+  qComment.equalTo('complaintId', complaint);
+  qComment.descending('updatedAt');
+  qComment.find({
     success: function(comments) {
-      
+      for (var i=0; i<comments.length; i++) {
+        var comment = comments[i];
+        appendComment(comment);
+      }
     },
     error: function(error) {
       console.log(error.message);
@@ -49,8 +53,7 @@ function retrieveVoteData(complaint) {
   qVoteUser.find({
     success: function(results) {
       $('#votes').text(results.length);
-      console.log(results.length);
-
+      
       //attach the upvote event
       $('#votes').one('click', function() {
         if (results.length==0) {
@@ -79,6 +82,49 @@ function upVote(VoteUser, complaint) {
     error: function(vote, error) {
       // Execute any logic that should take place if the save fails.
       // error is a Parse.Error with an error code and message.
+      alert('Failed to create new object, with error code: ' + error.message);
+    }
+  });
+}
+
+function setTagValue(idDom, complaint) {
+  var Label = Parse.Object.extend('Label');
+  var qLabel = new Parse.Query(Label);
+  qLabel.get('' + complaint.get("category").id, {
+    success: function(result) {
+      $(idDom).text( result.get('name') );
+    },
+    error: function(error) {
+      alert('Failed to retrieve tag name');
+    }
+  });
+}
+
+function appendComment(comment) {
+  var listSaAnswer = $('#sa-answer-list');
+  var listComment = $('#student-comment-list');
+
+  var qUser = new Parse.Query(Parse.User);
+  qUser.get(comment.get('commenterId').id, {
+    success: function(user) {
+      if (comment.get('isSaAnswer')) {
+        listSaAnswer.append(
+          '<div class="row sa-answer">' +
+            '<div class="col-md-2 sa-answer-netid">Answer by: ' + user.get('netId') + '</div>' +
+            '<div class="col-md-10 sa-answer-content">' + comment.get('Content') + '</div>' +
+          '</div>'
+        );
+      }
+      else {
+        listComment.append(
+          '<div class="row student-comment">' +
+            '<div class="col-md-2 student-comment-netid">Comment by: ' + user.get('netId') + '</div>' +
+            '<div class="col-md-10 student-comment-content">' + comment.get('Content') + '</div>' +
+          '</div>'
+        );
+      } 
+    },
+    error: function(user) {
       alert('Failed to create new object, with error code: ' + error.message);
     }
   });
